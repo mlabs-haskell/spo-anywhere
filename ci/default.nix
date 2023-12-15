eb42eae2a1a34741f20d033b17638e5f98f9f6a6
Andrea Ciceri <andrea.ciceri@autistici.org>
2023-12-15 18:18:17 +0100

    fix: import MLabs' `hercules-ci-effects` fork

--- ci/default.nix
+++ ci/default.nix
@@ -1,10 +1,12 @@
 {inputs, ...}: {
   imports = [
     inputs.hercules-ci-effects.flakeModule
+    (import "${inputs.hercules-ci-effects}/effects/populate-cache/default.nix")
   ];
   herculesCI.ciSystems = ["x86_64-linux" "x86_64-darwin"];
   hercules-ci.populate-cache-effect = {
     enable = true;
+    attic-client-pkg = inputs.attic.packages.x86_64-linux.attic-client;
     caches = {
       mlabs-spo-anywhere = {
         type = "attic";
--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     # TODO: use upstream `hercules-ci-effects` once this is merged:
     # https://github.com/hercules-ci/hercules-ci-effects/pull/163/
     # hercules-ci-effects.follows = "cardanoNix/hercules-ci-effects";
-    hercules-ci-effects.url = "github:zmrocze/hercules-ci-effects/karol/push-cache-effect";
+    hercules-ci-effects.url = "github:mlabs-haskell/hercules-ci-effects/push-cache-effect";
 
     devshell = {
       url = "github:numtide/devshell";
