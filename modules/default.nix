1e24ad7e6831d31e73f7c84aa7707975af9d3b57
zmrocze <karolochmanmilarski@gmail.com>
2024-01-23 15:08:52 +0100

    Most basic test for block producer

--- modules/default.nix
+++ modules/default.nix
@@ -1,25 +1,8 @@
-{ config, inputs, ...}: let 
-
-  relay-node = inputs.nixpkgs.lib.nixosSystem {
-    system = "x86_64-linux";
-    modules = [
-      config.flake.nixosModules.relay-node
-      {
-        services.pipewire.enable = false;
-        hardware.pulseaudio.enable = false;
-      }
-    ];
-  };
-
-in {
-
-  flake.nixosConfigurations = {
-    relay-node = relay-node;
-  };
+{ config, inputs, ...}: {
 
   flake.nixosModules = {
     module = ./dummy;
-    relay-node = (import ./relay-node { inherit inputs; });
+    block-producer-node = (import ./block-producer-node { inherit inputs; });
     # the default module imports all modules
     default = {
       imports = with builtins; attrValues (removeAttrs config.flake.nixosModules ["default"]);
--- tests/default.nix
+++ tests/default.nix
@@ -1,7 +1,9 @@
+{ config, ...}:
 {
   perSystem = _: {
     imports = [
       ./dummy.nix
+      (import ./block-producer.nix { node-module = config.flake.nixosModules.block-producer-node; })
     ];
   };
 }
