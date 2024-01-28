d200489a5802d90a69b1fd5d95dd75fd3462dcef
zmrocze <karolochmanmilarski@gmail.com>
2024-01-28 12:59:30 +0100

    Enable flake input update

--- ci/default.nix
+++ ci/default.nix
@@ -8,18 +8,29 @@
     ./all-flake-drvs.nix
     inputs.hercules-ci-effects.push-cache-effect
   ];
-  herculesCI.ciSystems = ["x86_64-linux" "x86_64-darwin"];
-  allDrvs.systems = [
-    "x86_64-linux"
-  ];
-  push-cache-effect = {
-    enable = true;
-    attic-client-pkg = inputs.attic.packages.x86_64-linux.attic-client;
-    caches = {
-      mlabs-spo-anywhere = {
-        type = "attic";
-        secretName = "spo-anywhere-cache-push-token";
-        packages = config.allDrvs.list;
+  config = {
+    allDrvs.systems = [
+      "x86_64-linux"
+    ];
+    herculesCI = {
+      ciSystems = ["x86_64-linux" "x86_64-darwin"];
+      flake-update = {
+        enable = true;
+        when = {
+          hour = [23];
+          dayOfWeek = ["Sun"];
+        };
+      };
+    };
+    push-cache-effect = {
+      enable = true;
+      attic-client-pkg = inputs.attic.packages.x86_64-linux.attic-client;
+      caches = {
+	mlabs-spo-anywhere = {
+          type = "attic";
+          secretName = "spo-anywhere-cache-push-token";
+          packages = config.allDrvs.list;
+	};
       };
     };
   };
