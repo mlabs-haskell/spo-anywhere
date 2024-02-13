6d1feb6854c9a4aae92313bf77f43df6f8b40f11
zmrocze <karolochmanmilarski@gmail.com>
2024-02-13 11:51:29 +0100

    Fix typo

--- ci/default.nix
+++ ci/default.nix
@@ -14,12 +14,12 @@
     ];
     herculesCI = {
       ciSystems = ["x86_64-linux" "x86_64-darwin"];
-      flake-update = {
-        enable = true;
-        when = {
-          hour = [23];
-          dayOfWeek = ["Sun"];
-        };
+    };
+    hercules-ci.flake-update = {
+      enable = true;
+      when = {
+        hour = [23];
+        dayOfWeek = ["Sun"];
       };
     };
     push-cache-effect = {
