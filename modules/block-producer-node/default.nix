c2536cd10328eccdefd1032e6d672410872dfc5e
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-04-26 19:07:35 +0200

    Use custom module for the test

--- modules/block-producer-node/default.nix
+++ modules/block-producer-node/default.nix
@@ -2,107 +2,21 @@
   config,
   lib,
   ...
-}:
-with lib;
-with builtins; let
+}: let
   cfg = config.services.block-producer-node;
-
-  # [{address : str, port : int}] -> topology file derivation
-  # mimicks topology.json format
-  # Topology where every peer has single access point. TODO: allow to overwrite
-  mkBlockProducerTopology = relayAddrs:
-    toFile "topology.json" (
-      toJSON
-      {
-        localRoots =
-          map (
-            addr: {
-              accessPoints = [addr];
-              advertise = false;
-              valency = 1;
-            }
-          )
-          relayAddrs;
-        publicRoots = [
-          {
-            accessPoints = [
-            ];
-            advertise = false;
-          }
-        ];
-        useLedgerAfterSlot = -1;
-      }
-    );
 in {
   imports = [
     inputs.cardano-node.nixosModules.cardano-node
-    # inputs.cardano-node.nixosModules.cardano-submit-api
   ];
+
+  # TODO consider not using a wrapper module if it's not necessary (we'll see in the future as the project shapes up)
 
   options = {
     services.block-producer-node = {
-      enable = mkEnableOption "Enable block producer cardano-node with some defaults.";
-      environment = mkOption {
-        type = types.enum (attrNames config.services.cardano-node.environments);
-        default = "preprod";
-        description = ''
-          environment node will connect to
-        '';
-      };
-      extraNodeConfig = mkOption {
-        type = types.attrs;
-        default = {};
-        description = ''
-          Attribute set of options passed to cardano node config.
-        '';
-        example = ''
-          {
-            ShelleyGenesisFile = ./my-genesis.yaml;
-          }
-        '';
-      };
-      relayAddrs = mkOption {
-        type = with types; listOf attrs;
-        description = ''
-          Addresses to our relay nodes. Will be added to nodes local roots. Provided as a list of { address : _, port : _ } attribute sets.
-        '';
-        example = ''
-          [
-            {
-              address = "x.x.x.x";
-              port = 3000;
-            }
-            {
-              address = "y.y.y.y";
-              port = 3000;
-            }
-          ]
-        '';
-        default = [];
-      };
-      key-paths.node-kes-skey = mkOption {
-        type = types.path;
-        description = ''
-          File path to a node KES private key file.
-          Give the file 400 permission for the `cardano-node` user.
-          !Warning!: Don't provide a derivation as then the key is public.
-        '';
-      };
-      key-paths.node-vrf-skey = mkOption {
-        type = types.path;
-        description = ''
-          File path to a node VRF private key file.
-          Give the file 400 permission for the `cardano-node` user.
-          !Warning!: Don't provide a derivation as then the key is public.
-        '';
-      };
-      key-paths.node-opcert-cert = mkOption {
-        type = types.path;
-        description = ''
-          File path to a node operational certificate private key file.
-          Give the file 400 permission for the `cardano-node` user.
-          !Warning!: Don't provide a derivation as then the key is public.
-        '';
+      enable = lib.mkEnableOption "Enable block producer cardano-node with some defaults.";
+      configFilesPath = lib.mkOption {
+        type = lib.types.path;
+        description = "Path to the network configuration directory";
       };
     };
   };
@@ -110,32 +24,15 @@ in {
   config = lib.mkIf cfg.enable {
     services.cardano-node = {
       enable = true;
-      # systemdSocketActivation = false;
       port = 3001;
-      hostAddr = "127.0.0.1";
-      # address = "0.0.0.0";
-      inherit (cfg) environment;
-      topology = mkBlockProducerTopology cfg.relayAddrs;
-      nodeConfig =
-        config.services.cardano-node.environments.${config.services.cardano-node.environment}.nodeConfig
-        // {
-          hasPrometheus = [config.services.cardano-node.hostAddr 12798];
-        }
-        // cfg.extraNodeConfig;
-      # Keys.
-      # [Idea1]: These options can be set from a "node-keys" module that works on top of the secrets module like agenix
-      # [Note2 (node-keys)]: It turned out the secret can't be provided by derivation because cardano-node complains.
-      #                      Solution provide in `etc` with copy mode (see [test/block-producer.nix]).
-      #
-      # Setting these should allow block production:
-      kesKey = cfg.key-paths.node-kes-skey;
-      vrfKey = cfg.key-paths.node-vrf-skey;
-      operationalCertificate = cfg.key-paths.node-opcert-cert;
-      # These are likely byron leftovers:
-      # signingKey = null;
-      # delegationCertificate = null;
+      hostAddr = "127.0.0.1"; # We don't want to expose the block producer
+      nodeConfigFile = "${cfg.configFilesPath}/configuration.yaml";
+      vrfKey = "${cfg.configFilesPath}/pools/vrf1.skey";
+      kesKey = "${cfg.configFilesPath}/pools/kes1.skey";
+      operationalCertificate = "${cfg.configFilesPath}/pools/opcert1.cert";
+      topology = "${cfg.configFilesPath}/topology-spo-1.json";
+      signingKey = "${cfg.configFilesPath}/byron-gen-command/delegate-keys.000.key";
+      delegationCertificate = "${cfg.configFilesPath}/byron-gen-command/delegation-cert.000.json";
     };
-    # restart after process exits? i GUESS poeple do this in tests
-    # systemd.services.cardano-node.serviceConfig.Restart = lib.mkForce "no";
   };
 }
--- tests/block-producer.nix
+++ tests/block-producer.nix
@@ -37,20 +37,15 @@
           }: {
             imports = [common];
 
+            # The cardano node requires that the configurations files and keys/certificates have the right permissions
             systemd.tmpfiles.rules = [
               "C+ /etc/testnet - - - - ${./local-testnet-config}"
               "Z /etc/testnet 700 cardano-node cardano-node - ${./local-testnet-config}"
             ];
 
-            services.cardano-node = {
+            services.block-producer-node = {
               enable = true;
-              nodeConfigFile = "/etc/testnet/configuration.yaml";
-              vrfKey = "/etc/testnet/pools/vrf1.skey";
-              kesKey = "/etc/testnet/pools/kes1.skey";
-              operationalCertificate = "/etc/testnet/pools/opcert1.cert";
-              topology = "/etc/testnet/topology-spo-1.json";
-              signingKey = "/etc/testnet/byron-gen-command/delegate-keys.000.key";
-              delegationCertificate = "/etc/testnet/byron-gen-command/delegation-cert.000.json";
+              configFilesPath = "/etc/testnet";
             };
 
             # This is a workaround to set a new start time for the ephemeral testnet created by the test
@@ -69,71 +64,71 @@
                 ];
                 runtimeEnv.CARDANO_NODE_SOCKET_PATH = "/run/cardano-node/node.socket";
                 text = ''
-                  trap 'echo "# $BASH_COMMAND"' DEBUG
+                              trap 'echo "# $BASH_COMMAND"' DEBUG
 
-                  cardano-cli query tip --testnet-magic 2
-                  cardano-cli query utxo --whole-utxo --testnet-magic 2
+                              cardano-cli query tip --testnet-magic 2
+                              cardano-cli query utxo --whole-utxo --testnet-magic 2
 
-                                cardano-cli address build \
-                                  --payment-verification-key-file "/etc/testnet/utxo-keys/utxo1.vkey" \
-                                  --out-file "/tmp/wallet.addr" \
-                                  --testnet-magic 2
+                              cardano-cli address build \
+                                --payment-verification-key-file "/etc/testnet/utxo-keys/utxo1.vkey" \
+                                --out-file "/tmp/wallet.addr" \
+                                --testnet-magic 2
 
-                                cardano-cli query utxo \
-                                  --testnet-magic 2 \
-                                  --address "$(cat "/tmp/wallet.addr")" \
-                                  --out-file "/tmp/utxos.json"
+                              cardano-cli query utxo \
+                                --testnet-magic 2 \
+                                --address "$(cat "/tmp/wallet.addr")" \
+                                --out-file "/tmp/utxos.json"
 
-                                TXIN=$(jq "keys[0]" "/tmp/utxos.json" --raw-output)
-                                SEND_AMT=3000000
-                  WALLET_ADDR="$(cat /tmp/wallet.addr)"
-                                TXOUT="$(cat "/tmp/wallet.addr")+$SEND_AMT"
+                              TXIN=$(jq "keys[0]" "/tmp/utxos.json" --raw-output)
+                              SEND_AMT=3000000
+                              WALLET_ADDR="$(cat /tmp/wallet.addr)"
+                              TXOUT="$(cat "/tmp/wallet.addr")+$SEND_AMT"
 
-                                cardano-cli transaction build \
-                    --testnet-magic 2 \
-                    --change-address "$WALLET_ADDR" \
-                    --tx-in "$TXIN" \
-                    --tx-out "$TXOUT" \
-                    --out-file "/tmp/tx.body" \
-                    --witness-override 2
+                              cardano-cli transaction build \
+                                --testnet-magic 2 \
+                                --change-address "$WALLET_ADDR" \
+                                --tx-in "$TXIN" \
+                                --tx-out "$TXOUT" \
+                                --out-file "/tmp/tx.body" \
+                                --witness-override 2
 
-                                cardano-cli transaction sign \
-                    --tx-body-file "/tmp/tx.body" \
-                    --signing-key-file "/etc/testnet/utxo-keys/utxo1.skey" \
-                    --testnet-magic 2 \
-                    --out-file "/tmp/tx.signed"
+                              cardano-cli transaction sign \
+                                --tx-body-file "/tmp/tx.body" \
+                                --signing-key-file "/etc/testnet/utxo-keys/utxo1.skey" \
+                  --testnet-magic 2 \
+                  --out-file "/tmp/tx.signed"
 
-                                cardano-cli query utxo \
-                                  --testnet-magic 2 \
-                    --address "$WALLET_ADDR" \
-                                  --out-file=/dev/stdout
+                              cardano-cli query utxo \
+                                --testnet-magic 2 \
+                                --address "$WALLET_ADDR" \
+                                --out-file=/dev/stdout
 
-                                echo "There is only 1 UTXO owned by the wallet"
-                  cardano-cli query utxo \
-                                  --testnet-magic 2 \
-                    --address "$WALLET_ADDR" \
-                                  --out-file=/dev/stdout \
-                  | jq -e "length == 1"
+                              echo "There is only 1 UTXO owned by the wallet"
+                              cardano-cli query utxo \
+                                --testnet-magic 2 \
+                                --address "$WALLET_ADDR" \
+                                --out-file=/dev/stdout \
+                              | jq -e "length == 1"
 
-                                cardano-cli transaction submit \
-                    --tx-file "/tmp/tx.signed" \
-                    --testnet-magic 2
+                              cardano-cli transaction submit \
+                                --tx-file "/tmp/tx.signed" \
+                                --testnet-magic 2
 
-                                while true; do
-                    CURRENT_BLOCK="$(cardano-cli query tip --testnet-magic 2 | jq '.block // 0')"
-                    if [ "$CURRENT_BLOCK" -eq 1 ]; then
-                                    echo "Waiting for a block to be mined..."
-                      break
-                    fi
-                    sleep 5
-                  done
+                              while true; do
+                                CURRENT_BLOCK="$(cardano-cli query tip --testnet-magic 2 | jq '.block // 0')"
+                                if [ "$CURRENT_BLOCK" -eq 1 ]; then
+                                   echo "Waiting for a block to be mined..."
+                                  break
+                                fi
+                                sleep 5
+                              done
 
-                                echo "Now there are 2 UTXOs"
-                  cardano-cli query utxo \
-                                  --testnet-magic 2 \
-                    --address "$WALLET_ADDR" \
-                                  --out-file=/dev/stdout \
-                  | jq -e "length == 2"
+                              echo "Now there are 2 UTXOs"
+                              cardano-cli query utxo \
+                                --testnet-magic 2 \
+                                --address "$WALLET_ADDR" \
+                                --out-file=/dev/stdout \
+                              | jq -e "length == 2"
                 '';
               })
             ];
