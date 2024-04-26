2a3f80ad00e1ee3b0dcadf5c746a093a68406332
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-04-26 19:08:29 +0200

    Remove unused module

--- modules/default.nix
+++ modules/default.nix
@@ -6,7 +6,6 @@
   flake.nixosModules = {
     module = ./dummy;
     block-producer-node = import ./block-producer-node {inherit inputs;};
-    relay-node = import ./relay-node {inherit inputs;};
     # the default module imports all modules
     default = {
       imports = with builtins; attrValues (removeAttrs config.flake.nixosModules ["default"]);
