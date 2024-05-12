a776438c85ed27d8b3d93ce3a23ee3441b3b9a7b
zmrocze <karolochmanmilarski@gmail.com>
2024-05-12 15:15:15 +0200

    Parse cmdline arguments in deploy script

--- modules/default.nix
+++ modules/default.nix
@@ -6,6 +6,7 @@
   flake.nixosModules = {
     module = ./dummy;
     block-producer-node = import ./block-producer-node {inherit inputs;};
+    dummy = import ./dummy;
     # the default module imports all modules
     default = {
       imports = with builtins; attrValues (removeAttrs config.flake.nixosModules ["default"]);
