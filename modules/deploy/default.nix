66c60d84149f9c41e65e3b74602e6ed867e5df7e
zmrocze <karolochmanmilarski@gmail.com>
2024-06-01 15:05:48 +0200

    Add ssh-key argument [update according to branch from iss24]

--- modules/deploy/default.nix
+++ modules/deploy/default.nix
@@ -1,8 +1,18 @@
-# This dummy module simply adds `hello` to the system PATH
-{ pkgs, config, ... }: {
-  config = {
-    system.build.spoDeployScript = pkgs.writeShellApplication {
-      name = "spo-deploy-script";
+# This module defines deployment script at `config.system.build.spoInstallScript`.
+{ pkgs, config, lib, ... }: {
+
+  options = {
+    spo-anywhere.install-script =  {
+      enable = lib.mkEnableOption "Create deployment script at `config.system.build.spoInstallScript`." //
+        {default = config.spo-anywhere.enable or false;};
+    };
+  };
+
+  config = let 
+    cfg = config.spo-anywhere.install-script;
+    in lib.mkIf cfg.enable {
+    system.build.spoInstallScript = pkgs.writeShellApplication {
+      name = "spo-install-script";
       runtimeInputs = with pkgs; [ nixos-anywhere getopt ];
       text = ''
         # shellcheck disable=SC2154
@@ -10,10 +20,10 @@
         # ### command parsing ###
 
         usage() {
-          echo "Usage: spo-deploy-script --target <target> --key-dir <key-dir>"
+          echo "Usage: spo-install-script --target <target> --ssh-key <filepath> --spo-keys <directory>"
         }
 
-        args="$(getopt --name spo-deploy-script -o 'h' --longoptions target:,key-dir: -- "$@")"
+        args="$(getopt --name spo-install-script -o 'h' --longoptions target:,ssh-key:,spo-keys: -- "$@")"
         eval set -- "$args"
         while true; do
           case "$1" in
@@ -21,8 +31,12 @@
                 target="$2"
                 shift 2
                   ;;
-            --key-dir)
-                key_dir="$2"
+            --ssh-key)
+                ssh_key="$2"
+                shift 2
+                ;;
+            --spo-keys)
+                spo_keys="$2"
                 shift 2
                 ;;
             -h|--help)
@@ -34,20 +48,23 @@
           esac
         done
 
-        if [ -z "''${target:+true}" ] || [ -z "''${key_dir:+true}" ]; then
+        if [ -z "''${target:+true}" ] || [ -z "''${spo_keys:+true}" ] || [ -z "''${ssh_key:+true}" ]; then
           usage
           exit 1
         fi
 
         # ### main ###
 
-        # here key_dir should be of form dir/path/to/where/spo/expects/keys.
+        # here spo_keys should be of form dir/path/to/where/spo/expects/keys.
         # Options:
         #   1. leave to user to care
         #   2. make our key generation commands, generate the keys in a directory of this form
         #   3. Copy to tmp/dir/path/to/where/spo/expects/keys. set cleanup hook
-        nixos-anywhere --store-paths ${config.system.build.toplevel} ${config.system.build.diskoScriptNoDeps} \
-          --extra-files "$key_dir" \
+        #   4. use scp instead
+        nixos-anywhere \
+          --store-paths ${config.system.build.toplevel} ${config.system.build.diskoScript} \
+          --extra-files "$spo_keys" \
+          -i "$ssh_key" \
           "$target"
       '';
     };
