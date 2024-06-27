0a2c15df923ba888c695d2d0faa049629793cccb
zmrocze <karolochmanmilarski@gmail.com>
2024-06-27 13:42:33 +0200

    Import virtualisation/digital-ocean

--- modules/disko.nix
+++ modules/disko.nix
@@ -1,7 +1,23 @@
-{disko, ...}: {...}: {
+{disko, ...}: {lib, modulesPath, ...}: {
   imports = [
     disko.nixosModules.disko
-  ];
+    "${modulesPath}/virtualisation/digital-ocean-config.nix"
+    # (modulesPath + "/profiles/all-hardware.nix")
+  ]; 
+
+  boot.initrd.kernelModules = [ "nvme" ];
+  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi" ];
+
+  # boot.loader.grub.device = "/dev/vda";
+
+  # fileSystems."/" = {
+  #   device = lib.mkForce "/dev/vda1";
+  #   fsType = "ext4";
+  # };
+
+  # networking.useDHCP = true;
+  # networking.firewall.enable = false;
+  users.users.root.password = "spo";
   disko.devices = {
     disk = {
       vda = {
@@ -10,19 +26,19 @@
         content = {
           type = "gpt";
           partitions = {
-            boot = {
-              size = "1M";
-              type = "EF02";
-            };
-            ESP = {
-              size = "100M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-              };
-            };
+            # boot = {
+            #   size = "1M";
+            #   type = "EF02";
+            # };
+            # ESP = {
+            #   size = "100M";
+            #   type = "EF00";
+            #   content = {
+            #     type = "filesystem";
+            #     format = "vfat";
+            #     mountpoint = "/boot";
+            #   };
+            # };
             root = {
               size = "100%";
               content = {
