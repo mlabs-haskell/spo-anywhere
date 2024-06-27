6f7f1649dd34c01be76a42b9480eeceb42c0f975
zmrocze <karolochmanmilarski@gmail.com>
2024-06-27 16:16:54 +0200

    Define hardware config

--- modules/disko.nix
+++ modules/disko.nix
@@ -2,22 +2,16 @@
   imports = [
     disko.nixosModules.disko
     "${modulesPath}/virtualisation/digital-ocean-config.nix"
-    # (modulesPath + "/profiles/all-hardware.nix")
+    (modulesPath + "/profiles/all-hardware.nix")
   ]; 
 
-  boot.initrd.kernelModules = [ "nvme" ];
-  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi" ];
+  boot.loader.grub = {
+    # no need to set devices, disko will add all devices that have a EF02 partition to the list already
+    # devices = [ ];
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+  };
 
-  # boot.loader.grub.device = "/dev/vda";
-
-  # fileSystems."/" = {
-  #   device = lib.mkForce "/dev/vda1";
-  #   fsType = "ext4";
-  # };
-
-  # networking.useDHCP = true;
-  # networking.firewall.enable = false;
-  users.users.root.password = "spo";
   disko.devices = {
     disk = {
       vda = {
@@ -26,19 +20,19 @@
         content = {
           type = "gpt";
           partitions = {
-            # boot = {
-            #   size = "1M";
-            #   type = "EF02";
-            # };
-            # ESP = {
-            #   size = "100M";
-            #   type = "EF00";
-            #   content = {
-            #     type = "filesystem";
-            #     format = "vfat";
-            #     mountpoint = "/boot";
-            #   };
-            # };
+            boot = {
+              size = "1M";
+              type = "EF02";
+            };
+            ESP = {
+              size = "100M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
             root = {
               size = "100%";
               content = {
