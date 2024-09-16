610f6162c509583f0cc5f1698238b043db5c9ac1
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-09-16 14:01:58 +0200

    Move template in a separate directory

--- flake.nix
+++ flake.nix
@@ -3,12 +3,10 @@
     # FIXME more recent revisions breaks the docs
     nixpkgs.url = "github:NixOS/nixpkgs/58a1abdbae3217ca6b702f03d3b35125d88a2994";
     flake-parts.url = "github:hercules-ci/flake-parts";
-
     # TODO: use upstream `hercules-ci-effects` once this is merged:
     # https://github.com/hercules-ci/hercules-ci-effects/pull/165/
     # hercules-ci-effects.follows = "cardanoNix/hercules-ci-effects";
     hercules-ci-effects.url = "github:mlabs-haskell/hercules-ci-effects/push-cache-effect";
-
     devshell = {
       url = "github:numtide/devshell";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -46,14 +44,11 @@
         ./modules
         ./tests
         ./apps
+        ./templates
       ];
       systems = [
         "x86_64-linux"
         "x86_64-darwin"
-      ];
-      flake.templates.default = {
-        path = ./template;
-        description = "Example flake using spo-anywhere";
-      };
+      ];   
     };
 }
